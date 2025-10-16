9281005ef6964a17e85dd3e4dcf4a3f51ca89852
sebastian avella calderon <sebastian.avella@upb.edu.co>
2025-10-16 17:34:19 -0500

    Ejercicio de practica 1.

--- leer1.py
+++ leer1.py
@@ -5,6 +5,10 @@ fp = open("C:\\Users\\B09S202est\\Desktop\\archivo1.docx", "r", encoding="utf-8"
 #datos = fp.read()
 datos = fp.readline()
 #3. Cerrar el archivo
+# cadena = "Hola"
+# cadena[1]
+for linea in fp:
+    print(linea,end="")
 fp.close()
 
 print(datos)
